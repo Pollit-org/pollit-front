a9405f157cdbffac6b1d127746666ff97c8c68b8
MrFLow <comte.florian@gmail.com>
2023-01-13 15:31:36 +0100

    google signin backend integration

---
+++ src/axios/index.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosError } from 'axios';
+import router from '@/router';
+
+const axiosPollit = axios.create({
+  // baseURL: 'https://dev.auth.pollit.me',
+  baseURL: 'https://localhost:7019',
+});
+
+  const unauthorizedResponseInterceptor = (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('accessToken');
+      router.go(0);
+    }
+
+    return Promise.reject(error);
+  };
+
+  axiosPollit.interceptors.response.use(response => response, unauthorizedResponseInterceptor);
+
+export { axiosPollit };
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -45,6 +45,6 @@ export default createVuetify({
     aliases,
     sets: {
       mdi,
-    }
+    },
   },
 })
