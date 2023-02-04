2b0f410876daf4828891128cbf75f9290d2f1503
Florian Comte <comte.florian@gmail.com>
2023-02-04 17:35:23 +0100

    PollitPage

---
+++ src/components/PollitPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="flex justify-center">
+      <div style="width: 80%; max-width: 650px;">
+          <slot></slot>
+      </div>
+    </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
