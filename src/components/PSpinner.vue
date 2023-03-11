ee1964996733b20f90ced1cfa16886e0e0432df8
Florian Comte <comte.florian@gmail.com>
2023-03-11 11:45:38 +0100

    IDisposable loader and spinner page component

---
+++ src/components/PSpinner.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-spinner-audio color="primary" size="2em" />
+</template>
+  
+<script setup lang="ts">
+</script>
+  
---
+++ src/components/PSpinnerPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <pollit-page
+    vertical-center
+    class="text-center"
+    max-width="340px"
+    :show-spinner-on-load="false"
+  >
+    <p-spinner></p-spinner>
+  </pollit-page>
+</template>
+
+<script setup lang="ts">
+import PollitPage from 'components/PollitPage.vue';
+import PSpinner from 'components/PSpinner.vue';
+</script>
