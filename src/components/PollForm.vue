b66cb8f7c7ded1d58770294588cbf7b7089fc929
Matthieu-Lormeau <matthieulormeau@gmail.com>
2023-04-10 15:31:38 +0200

    style form

--- src/components/PollForm.vue
+++ src/components/PollForm.vue
@@ -1,36 +1,31 @@
 <template>
   <q-form @submit="submitForm">
-    <q-card>
-      <q-card-section class="q-pb-none" :horizontal="false">
-        <q-input
-          filled
-          v-model="question"
-          label="Just poll it."
-          lazy-rules
-          :rules="[
-            (val) => !!val || 'Question is required',
-            (val) =>
-              (val && val.length >= 3) || 'Question must be at least 3 characters long',
-          ]"
-        />
-      </q-card-section>
-      <q-card-section>
-        <q-list bordered>
-          <q-item v-for="(option, index) in options" :key="index">
-            <q-item-section>
+    <q-card bordered>
+      <q-input
+        dense
+        class="q-pt-md q-pl-md q-pb-none"
+        v-model="question"
+        label="Just poll it..."
+        lazy-rules="ondemand"
+        :rules="[(val) => /\S/.test(val) || 'Question is required']"
+      />
+      <q-card-section class="q-pb-xs">
+        <q-list bordered dense>
+          <q-item v-for="(option, index) in options" :key="index" dense>
+            <q-item-section dense>
               <q-input
+                size="xs"
+                class="q-pb-sm"
                 v-model="options[index]"
                 :placeholder="'option ' + (index + 1)"
-                lazy-rules
-                :rules="[
-                  (val) => !!val || 'Option can\'t be blank',
-                  (val) =>
-                    (val && val.length >= 3) ||
-                    'Option must be at least 3 characters long',
-                ]"
+                lazy-rules="ondemand"
+                :rules="[(val) => /\S/.test(val) || 'Option can\'t be blank']"
                 :disable="index === options.length - 1"
                 dense
               />
+              <q-tooltip v-if="index === options.length - 1"
+                >"{{ default_option_text }}" default option can't be removed.</q-tooltip
+              >
             </q-item-section>
             <q-item-section
               side
@@ -39,19 +34,20 @@
               <q-btn dense flat icon="close" @click="removeOption(index)" />
             </q-item-section>
           </q-item>
+          <q-btn
+            v-if="options.length <= 10"
+            dense
+            size="xs"
+            color="primary"
+            class="q-ma-sm"
+            icon="add"
+            @click="addOption"
+          />
         </q-list>
-        <q-btn
-          v-if="options.length <= 10"
-          dense
-          color="primary"
-          class="q-mt-sm"
-          icon="add"
-          label="Add Option"
-          @click="addOption"
-        />
       </q-card-section>
       <q-card-actions>
         <q-btn
+          size="sm"
           type="submit"
           color="primary"
           label="Create Poll"
@@ -62,38 +58,28 @@
   </q-form>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
+<script setup lang="ts">
+import { ref } from 'vue';
 
-export default defineComponent({
-  setup() {
-    const question = ref('');
-    const options = ref(['', '', "I don't know"]);
+const default_option_text = "I don't know";
+const question = ref('');
+const options = ref(['', '', default_option_text]);
 
-    const addOption = () => {
-      options.value.splice(options.value.length - 1, 0, ''); // add a blank option before the "I don't know" option
-    };
+const addOption = () => {
+  options.value.splice(options.value.length - 1, 0, ''); // add a blank option before the "I don't know" option
+};
 
-    const removeOption = (index: number) => {
-      if (index === options.value.length - 1 || options.value.length <= 3) {
-        // if trying to remove the "I don't know" option, do nothing
-        return;
-      }
-      options.value.splice(index, 1);
-    };
+const removeOption = (index: number) => {
+  if (index === options.value.length - 1 || options.value.length <= 3) {
+    // if trying to remove the "I don't know" option, or having less than 2 voting options
+    // do nothing
+    return;
+  }
+  options.value.splice(index, 1);
+};
 
-    const submitForm = () => {
-      // Handle form submission here
-      console.log('Form submitted!');
-    };
-
-    return {
-      question,
-      options,
-      addOption,
-      removeOption,
-      submitForm,
-    };
-  },
-});
+const submitForm = () => {
+  // Handle form submission here
+  console.log('Form submitted!');
+};
 </script>
